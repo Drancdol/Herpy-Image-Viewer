f105303d5be94f4fe92105dda48706d9be917c10
Drancdol <Drancdol@163.com>
2026-07-20 15:03:02 +0800

    Added multiple request header fields

--- src/apis/request.ts
+++ src/apis/request.ts
@@ -60,8 +60,24 @@ export const apiClient = axios.create({
 
 apiClient.interceptors.request.use((config: InternalAxiosRequestConfig) => {
   const headers = AxiosHeaders.from(config.headers);
-  headers.set('Accept', 'text/html,application/xhtml+xml');
-
+  headers.set(
+    'Accept',
+    'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+  );
+  headers.set('Accept-Encoding', 'gzip, deflate');
+  headers.set(
+    'Accept-Language',
+    'zh-CN,zh;q=0.9,en-US;q=0.8,en;q=0.7,en-GB;q=0.6',
+  );
+  headers.set('Cache-Control', 'no-cache');
+  headers.set('Connection', 'keep-alive');
+  headers.set('Host', 'herpy.nu');
+  headers.set('Pragma', 'no-cache');
+  headers.set('Upgrade-Insecure-Requests', '1');
+  headers.set(
+    'User-Agent',
+    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/150.0.0.0 Safari/537.36 Edg/150.0.0.0',
+  );
   const cookieHeader = getAuthorizationCookieHeader();
   if (cookieHeader) {
     headers.set('Cookie', cookieHeader);
